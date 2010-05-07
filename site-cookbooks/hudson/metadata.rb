6cf6716dfcd3f79d5f6f205815fd5815c2fdfa41
Ben Mabey <ben@benmabey.com>
2010-05-06 19:13:26 -0600

    updates license for hudson cookbook to MIT

--- site-cookbooks/hudson/metadata.rb
+++ site-cookbooks/hudson/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Ben Mabey"
 maintainer_email "ben@benmabey.com"
-license          "Apache 2.0"
+license          "MIT"
 description      "Installs/Configures hudson"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
@@ -8,3 +8,4 @@ version          "0.1"
 %w[apt nginx].each do |cb|
   depends cb
 end
+supports "ubuntu"
