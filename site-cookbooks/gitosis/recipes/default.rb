ed4f83e4b92d898c882899066218c47a51033b98
Ben Mabey <ben@benmabey.com>
2010-05-06 20:28:47 -0600

    adds guard for gitosis package install
    gitosis package call was failing when trying to reinstall thus halting chef

--- site-cookbooks/gitosis/recipes/default.rb
+++ site-cookbooks/gitosis/recipes/default.rb
@@ -1,5 +1,6 @@
-package "gitosis"
-
+package "gitosis" do
+  not_if "test -f /srv/gitosis/.gitosis.conf"
+end
 execute "initalize gitosis" do
   command "sudo -H -u gitosis gitosis-init < /home/#{node[:gitosis][:admin]}/.ssh/authorized_keys"
   not_if "test -f /srv/gitosis/.gitosis.conf"
