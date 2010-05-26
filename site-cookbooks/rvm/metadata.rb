41fb6269029f714d32339e8b3f9378c1fcf12de9
Ben Mabey <ben@benmabey.com>
2010-05-26 17:59:56 -0600

    brings in rvm cookbook from cider

---
+++ site-cookbooks/rvm/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "atmos.org"
+maintainer_email "atmos@atmos.org"
+license          "MIT"
+description      "Installs/Configures rvm"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1.0"
