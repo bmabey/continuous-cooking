8d140f1bcdd6a2e4d81d89a09581f2d439bc2fcc
Ben Mabey <ben@benmabey.com>
2010-05-29 13:04:16 -0600

    extacts hudson rvm_user settings into attributes

--- site-cookbooks/hudson/attributes/default.rb
+++ site-cookbooks/hudson/attributes/default.rb
@@ -2,5 +2,9 @@ set_unless[:hudson][:port]  = 8080
 set_unless[:hudson][:domain]  = "hudson.codebox"
 set_unless[:hudson][:git_user_name]  = "Hudson"
 set_unless[:hudson][:plugins]  = {"greenballs" => "1.6", "git" => "0.8.3"}
-set_unless[:hudson][:apt_repository]  = "hudson-labs"
+set_unless[:hudson][:apt_repository]  = "hudson-labs" # this version has the most recent builds
 
+set_unless[:hudson][:rvm][:rubies] = %w[1.8.7-p248]
+set_unless[:hudson][:rvm][:packages] = %w[zlib]
+set_unless[:hudson][:rvm][:default_ruby] = "1.8.7-p248"
+set_unless[:hudson][:rvm][:gems] = {:all_versions => {'bundler' => '0.9.25', 'bundler08' => '0.8.5'}}
--- site-cookbooks/hudson/recipes/ruby.rb
+++ site-cookbooks/hudson/recipes/ruby.rb
@@ -2,10 +2,10 @@ include_recipe "hudson"
 
 rvm_user do
   user "hudson"
-  rubies %w[1.8.7-p248]
-  packages %w[zlib]
-  default_ruby "1.8.7-p248"
-  gems :all_versions => {'bundler' => '0.9.25', 'bundler08' => '0.8.5'}
+  rubies node[:hudson][:rvm][:rubies]
+  packages []
+  default_ruby node[:hudson][:rvm][:default_ruby]
+  gems node[:hudson][:rvm][:gems]
 end
 
 hudson_plugin "ruby" => "1.2", "rubyMetrics" => "1.4", "rake" => "1.6.3"
