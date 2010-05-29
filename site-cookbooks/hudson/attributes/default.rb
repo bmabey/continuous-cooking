0d3ded19dd2448b14896ec49d2f6cb990c9fd725
Ben Mabey <ben@benmabey.com>
2010-05-29 02:06:18 -0600

    adds hudson-labs apt repo (the latest hudson builds) and makes default

--- site-cookbooks/hudson/attributes/default.rb
+++ site-cookbooks/hudson/attributes/default.rb
@@ -2,3 +2,5 @@ set_unless[:hudson][:port]  = 8080
 set_unless[:hudson][:domain]  = "hudson.codebox"
 set_unless[:hudson][:git_user_name]  = "Hudson"
 set_unless[:hudson][:plugins]  = {"greenballs" => "1.6", "git" => "0.8.3"}
+set_unless[:hudson][:apt_repository]  = "hudson-labs"
+
--- site-cookbooks/hudson/recipes/default.rb
+++ site-cookbooks/hudson/recipes/default.rb
@@ -17,13 +17,14 @@ when "ubuntu"
   include_recipe "apt"
   package "wget"
 
-  execute "wget -O - http://hudson-ci.org/debian/hudson-ci.org.key | apt-key add -" do
+  apt_repo = node[:hudson][:apt_repository]
+  execute "wget -O - http://#{apt_repo}.org/debian/#{apt_repo}.org.key | apt-key add -" do
     not_if "apt-key finger | grep '150F DE3F 7787 E7D1 1EF4  E12A 9B7D 32F2 D505 82E6'"
   end
 
-  remote_file "/etc/apt/sources.list.d/huson-ci.org.list" do
+  remote_file "/etc/apt/sources.list.d/#{apt_repo}.org.list" do
     mode "0644"
-    source "hudson-ci.org.list"
+    source "#{apt_repo}.org.list"
     notifies :run, resources(:execute => "apt-get update"), :immediately
   end
 end
