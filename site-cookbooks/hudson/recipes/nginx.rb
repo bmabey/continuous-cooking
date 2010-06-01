46a29f579f05d884b170f5b9db3bb036d8c6824f
Ben Mabey <ben@benmabey.com>
2010-06-01 10:51:23 -0600

    removes cruft

--- site-cookbooks/hudson/recipes/nginx.rb
+++ site-cookbooks/hudson/recipes/nginx.rb
@@ -3,7 +3,6 @@ include_recipe "nginx"
 
 nginx_vhost_proxy node[:hudson][:domain] do
   ssl_only false
-  #root "#{lt_appraiser_dir}/current/public/"
   upstream ["127.0.0.1:#{node[:hudson][:port]}"]
 end
 
