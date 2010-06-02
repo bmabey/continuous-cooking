2b306a90ccc429f44de76324e53df6a06e676bd7
Ben Mabey <ben@benmabey.com>
2010-06-02 13:52:53 -0600

    fixes hudson CB bootstapping issues, HOME bug, and RVM gem settings

--- site-cookbooks/hudson/attributes/default.rb
+++ site-cookbooks/hudson/attributes/default.rb
@@ -7,4 +7,4 @@ set_unless[:hudson][:apt_repository]  = "hudson-labs" # this version has the mos
 set_unless[:hudson][:rvm][:rubies] = %w[1.8.7-p248]
 set_unless[:hudson][:rvm][:packages] = %w[zlib]
 set_unless[:hudson][:rvm][:default_ruby] = "1.8.7-p248"
-set_unless[:hudson][:rvm][:gems] = {:all_versions => {'bundler' => '0.9.25', 'bundler08' => '0.8.5'}}
+set_unless[:hudson][:rvm][:gems] = {"all_versions" => {'bundler' => '0.9.25', 'bundler08' => '0.8.5'}}
--- site-cookbooks/hudson/recipes/default.rb
+++ site-cookbooks/hudson/recipes/default.rb
@@ -1,3 +1,21 @@
+user "hudson" do
+  home "/home/hudson"
+  shell "/bin/bash"
+end
+
+directory "/home/hudson" do
+  recursive true
+  owner "hudson"
+  mode 0700
+end
+
+# git requires that a user be set to checkout...
+template "/home/hudson/.gitconfig" do
+  owner "hudson"
+  source "dot.gitconfig.erb"
+  only_if "which git"
+end
+
 case node.platform
 when "ubuntu"
   include_recipe "apt"
@@ -8,9 +26,11 @@ when "ubuntu"
     not_if "apt-key finger | grep '150F DE3F 7787 E7D1 1EF4  E12A 9B7D 32F2 D505 82E6'"
   end
 
-  remote_file "/etc/apt/sources.list.d/#{apt_repo}.org.list" do
+  remote_file "/etc/apt/sources.list.d/huson-ci.org.list" do
     mode "0644"
     source "#{apt_repo}.org.list"
+    #checksum "eb072c6240d248e8343e3724595865c6c68bb42f5838762b5389edefdd917759"
+    #not_if "test -f /etc/apt/sources.list.d/#{apt_repo}.org.list"
     notifies :run, resources(:execute => "apt-get update"), :immediately
   end
 end
@@ -26,19 +46,6 @@ service "hudson" do
 end
 
 hudson_plugin node[:hudson][:plugins]
-
-directory "/home/hudson" do
-  recursive true
-  owner "hudson"
-  mode 0700
-end
-
-# git requires that a user be set to checkout...
-template "/home/hudson/.gitconfig" do
-  owner "hudson"
-  source "dot.gitconfig.erb"
-  only_if "which git"
-end
 
 link "/home/hudson/lib" do
   to "/var/lib/hudson"
