a82bc98bf275d2ea4cf2c419ae180fa695794a8f
Ben Mabey <ben@benmabey.com>
2010-06-16 14:37:30 -0600

    adds basic auth support in nginx proxy confs...

--- site-cookbooks/nginx/definitions/nginx_vhost_proxy.rb
+++ site-cookbooks/nginx/definitions/nginx_vhost_proxy.rb
@@ -1,4 +1,4 @@
-define :nginx_vhost_proxy, :log_path => "/var/log/nginx/", :root => nil, :ssl => false, :ssl_only => false, :listen => nil, :upstream => [] do
+define :nginx_vhost_proxy, :log_path => "/var/log/nginx/", :root => nil, :ssl => false, :ssl_only => false, :listen => nil, :upstream => [], :auth_basic => nil, :auth_basic_user_file => nil do
 
   name = params[:name]
   listen ||= (params[:ssl_only] ? [443] : (params[:ssl] ? [80, 443] : [80]))
@@ -13,16 +13,18 @@ define :nginx_vhost_proxy, :log_path => "/var/log/nginx/", :root => nil, :ssl =>
     group "root"
     mode 0644
     variables(
-      :name             => name,
-      :error_log        => "#{params[:log_path]}/#{name}-error.log",
-      :access_log       => "#{params[:log_path]}/#{name}-access.log",
-      :domains          => params[:domains],
-      :root             => params[:root],
-      :listen           => listen,
-      :upstream         => params[:upstream],
-      :ssl              => params[:ssl] || params[:ssl_only],
-      :ssl_cert         => params[:ssl_cert] || "/etc/ssl_certs/#{name}.crt",
-      :ssl_cert_key     => params[:ssl_cert_key] || "/etc/ssl_certs/#{name}.key",
+      :name                 => name,
+      :error_log            => "#{params[:log_path]}/#{name}-error.log",
+      :access_log           => "#{params[:log_path]}/#{name}-access.log",
+      :domains              => params[:domains],
+      :root                 => params[:root],
+      :listen               => listen,
+      :upstream             => params[:upstream],
+      :ssl                  => params[:ssl] || params[:ssl_only],
+      :ssl_cert             => params[:ssl_cert] || "/etc/ssl_certs/#{name}.crt",
+      :ssl_cert_key         => params[:ssl_cert_key] || "/etc/ssl_certs/#{name}.key",
+      :auth_basic           => params[:auth_basic],
+      :auth_basic_user_file => params[:auth_basic_user_file],
       :params     => params
     )
     notifies :restart, resources(:service => "nginx"), :delayed
