4b327ac8832ab6a07dee56a58def74584ced5cd4
Michael Guterl <mguterl@gmail.com>
2010-12-24 05:53:36 +0800

    add hudson::headless recipe for setting up xvfb and firefox

--- site-cookbooks/vagrant_main/recipes/default.rb
+++ site-cookbooks/vagrant_main/recipes/default.rb
@@ -1,6 +1,7 @@
 package "git-core"
 require_recipe "hudson::nginx"
 require_recipe "hudson::ruby"
+require_recipe "hudson::headless"
 require_recipe "gitosis"
 require_recipe "postfix"
 require_recipe "gerrit::nginx"
